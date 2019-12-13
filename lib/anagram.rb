5307d38c41207206ee97feb577a71ecc426ce42c
Student <example@example.com>
2019-12-13 10:29:05 -0800

    begin branch logic to assess if entered word is a valid input

---
+++ lib/anagram.rb
@@ -0,0 +1,13 @@
+class AnagramTest
+  def initialize(word)
+    @word_one = word_one
+    @word_two = word_two
+  end
+
+  if (word_one.class === Integer)
+    puts "Sorry, please enter real word"
+  end
+
+
+
+end
