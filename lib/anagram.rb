5dd833b77458b026d3b732f52660a6ab6270670e
Student <example@example.com>
2019-12-13 12:09:42 -0800

    beginning assessing letters being pushed to check array.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,6 +2,7 @@ class AnagramTest
   def initialize(word_one)
     @word_arr1 = word_one.split("")
     @word_arr2 = word_two.split("")
+    @word_check =[]
   end
 
   def word_one
@@ -9,6 +10,11 @@ class AnagramTest
   end
 
   def anagram_test()
-    word_arr1.each |letter| do
+    @word_arr1.each  do |letter|
+      if (word_arr2.include?(letter))
+        @word_check.push(letter)
+        return @word_check
+      end
+      puts @word_check
     end
   end
