7c4cba7df7fe4cefe399f317c02c88ed4d98dbaa
Student <example@example.com>
2019-12-13 15:07:51 -0800

    now be able to compare array lengths to determine if they are a match.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,20 @@
 class AnagramTest
   def initialize(word_one, word_two)
-    @word_arr1 = word_one.split
-    @word_arr2 = word_two.split
+    @word_arr1 = word_one.downcase.split(" ")
+    @word_arr2 = word_two.downcase.split(" ")
     @word_check =[]
   end
 
   def anagram_test()
-    @word_arr1.each do |letter|
-      if @word_arr2.include?(letter)
-        @word_check.push(letter)
-        return @word_arr1
-        
-      elsif @word_check.length > @word_arr1.length || @word_check.length < @word_arr1.length
-        return "This is an Anti-gram"
+    @word_arr1.each do |object|
+      if @word_arr2.include?(object)
+        @word_check.push(object)
+      end
+
+      if @word_check.length === @word_arr1.length
+        return "this is an anagram"
       else
-        return "This is an Anagram"
+        return "this is an Anti-gram"
       end
     end
   end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,6 +5,10 @@ require ('anagram')
 describe("testing player choice") do
   it("will test the class of word") do
     word = AnagramTest.new("tea", "eat")
-    expect(word.anagram_test()).to(eq("eat"))
+    expect(word.anagram_test()).to(eq("this is an Anti-gram"))
+  end
+  it("will test the class of word") do
+    word2 = AnagramTest.new("e", "e")
+    expect(word2.anagram_test()).to(eq("this is an anagram"))
   end
 end
