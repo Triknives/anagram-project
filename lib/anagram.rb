2e3e28888c69017028287e1a7dc75883c7b26249
Student <example@example.com>
2019-12-13 12:49:17 -0800

    will now begin testing downcasing on words provided

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,16 @@
 class AnagramTest
-  def initialize(word_one)
+  def initialize(word_one, word_two)
     @word_arr1 = word_one.split("")
     @word_arr2 = word_two.split("")
     @word_check =[]
   end
 
-  def word_one
-    @word_one
-  end
-
   def anagram_test()
-    @word_arr1.each  do |letter|
-      if (word_arr2.include?(letter))
+    @word_arr1.each do |letter|
+      if (@word_arr2.include?(letter))
         @word_check.push(letter)
-        return @word_check
       end
-      puts @word_check
+      @word_check
     end
   end
+end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,14 +3,8 @@ require ('anagram')
 
 
 describe("testing player choice") do
-
   it("will test the class of word") do
-    word = AnagramTest.new("Tea")
+    word = AnagramTest.new("tea", "eat")
     expect(word.anagram_test()).to(eq("tea"))
   end
-  it("will downcase given words") do
-    word1 = AnagramTest.new("Tea")
-    expect(word.anagram_test()).to(eq("tea"))
-  end
-
 end
