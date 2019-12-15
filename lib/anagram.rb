1a3cacea0f077e0ff11f853725795001fe08a1f9
Triknives <50305423+Triknives@users.noreply.github.com>
2019-12-14 18:00:00 -0800

    add additional tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class AnagramTest
   end
 
 
-  #Assesses matching letters within words provided, then creates new array to to allow for further comparison of letters contained in both.
+  #This while / do below will allow me to run it against the spec "must have a vowel" to be a real word, as vowel count must be > 0
   def anagram_test()
     i = 0
     while i < @word_arr1.length do
@@ -17,11 +17,11 @@ class AnagramTest
       i += 1
     end
     puts @vowel_count
-    
+  #Will now take vowel count and run it against our "real word" conditional.
     if @vowel_count == 0
       return "this is not a real word, KID"
     end
-
+    #Assesses matching letters within words provided, then creates new array to to allow for further comparison of letters contained in both.
     @word_arr1.each do |object|
       if @word_arr2.include?(object)
         @word_check.push(object)
@@ -29,9 +29,9 @@ class AnagramTest
     end
 
     if @word_check.length === @word_arr1.length
-      return "this is an Anagram"
+      return "These words are Anagrams"
     else
-      return "this is an Anti-gram"
+      return "These words are Anti-grams"
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,20 +3,24 @@ require ('anagram')
 
 
 describe("testing player choice") do
-  it("will test if there are matching letters shared") do
+  it("This will test if the word is an anagram") do
     test = AnagramTest.new("tea", "eta")
-    expect(test.anagram_test()).to(eq("this is an Anagram"))
+    expect(test.anagram_test()).to(eq("These words are Anagrams"))
   end
   it("continue to test array length") do
     test2 = AnagramTest.new("cat", "tac")
-    expect(test2.anagram_test()).to(eq("this is an Anagram"))
+    expect(test2.anagram_test()).to(eq("These words are Anagrams"))
   end
   it("will test if catches Anti-gram") do
     test3 = AnagramTest.new("adfea", "eat")
-    expect(test3.anagram_test()).to(eq("this is an Anti-gram"))
+    expect(test3.anagram_test()).to(eq("These words are Anti-grams"))
   end
   it("will test if catches Anti-gram") do
-    test4 = AnagramTest.new("tttt", "tttt")
-    expect(test4.anagram_test()).to(eq("this is not a real word, KID"))
+    test4 = AnagramTest.new("aets", "eat")
+    expect(test4.anagram_test()).to(eq("These words are Anti-grams"))
+  end
+  it("This will check if the input entered is real word") do
+    test5 = AnagramTest.new("tttt", "tttt")
+    expect(test5.anagram_test()).to(eq("this is not a real word, KID"))
   end
 end
