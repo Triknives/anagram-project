afc7149248fbb82cbcbd61ebda7cb8d01411be13
Triknives <50305423+Triknives@users.noreply.github.com>
2019-12-14 18:15:21 -0800

    update comments, and spacing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,10 @@
 class AnagramTest
   def initialize(word_one, word_two)
-    @word_arr1 = word_one.downcase.split("")
-    @word_arr2 = word_two.downcase.split("")
+    @word_arr1 = word_one.downcase.lstrip.split("")
+    @word_arr2 = word_two.downcase.lstrip.split("")
     @word_check =[]
     @vowel_count = 0
   end
-
 
   #This while / do below will allow me to run it against the spec "must have a vowel" to be a real word, as vowel count must be > 0
   def anagram_test()
@@ -16,18 +15,18 @@ class AnagramTest
       end
       i += 1
     end
-    puts @vowel_count
-  #Will now take vowel count and run it against our "real word" conditional.
+    
+    #Will now take vowel count and run it against our "real word" conditional.
     if @vowel_count == 0
       return "this is not a real word, KID"
     end
+
     #Assesses matching letters within words provided, then creates new array to to allow for further comparison of letters contained in both.
     @word_arr1.each do |object|
       if @word_arr2.include?(object)
         @word_check.push(object)
       end
     end
-
     if @word_check.length === @word_arr1.length
       return "These words are Anagrams"
     else
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -23,4 +23,8 @@ describe("testing player choice") do
     test5 = AnagramTest.new("tttt", "tttt")
     expect(test5.anagram_test()).to(eq("this is not a real word, KID"))
   end
+  it("This will check if the input entered can be assessed when having spaces in them") do
+    test6 = AnagramTest.new("I Eat", "I Tea")
+    expect(test6.anagram_test()).to(eq("These words are Anagrams"))
+  end
 end
