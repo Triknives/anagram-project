dcf4f90d35baf84c67db9681b50a819c070c3678
Triknives <50305423+Triknives@users.noreply.github.com>
2019-12-14 17:45:49 -0800

    update structure to allow complete pass through all conditions, to better assess each spec.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,30 +9,25 @@ class AnagramTest
 
   #Assesses matching letters within words provided, then creates new array to to allow for further comparison of letters contained in both.
   def anagram_test()
+    i = 0
+    while i < @word_arr1.length do
+      if @word_arr1[i] == "a" || @word_arr1[i] == "e" || @word_arr1[i] == "i" || @word_arr1[i] == "o" || @word_arr1[i] == "u"
+        @vowel_count += 1
+      end
+      i += 1
+    end
+    puts @vowel_count
+    
+    if @vowel_count == 0
+      return "this is not a real word, KID"
+    end
+
     @word_arr1.each do |object|
       if @word_arr2.include?(object)
         @word_check.push(object)
       end
     end
 
-    #this is an approach I came across a few times online, utilizing a "counter" to track vowels. This will allow me to run it against the spec "must have a vowel" to be a real word, as vowel count must be >
-    def vowel_check()
-      i = 0
-      while i < @word_arr1.length do
-        if @word_arr1[i] == "a" || @word_arr1[i] == "e" || @word_arr1[i] == "i" || @word_arr1[i] == "o" || @word_arr1[i] == "u"
-          @vowel_count += 1
-        end
-        i += 1
-      end
-      return @vowel_count
-    end
-    def real_word_check()
-      if @vowel_count == 0
-        return "this is not a real word, KID"
-      end
-    end
-
-    #assesses the length of array to determine all needed letters were pushed and match original length
     if @word_check.length === @word_arr1.length
       return "this is an Anagram"
     else
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,23 +4,19 @@ require ('anagram')
 
 describe("testing player choice") do
   it("will test if there are matching letters shared") do
-    word = AnagramTest.new("tea", "eat")
-    expect(word.anagram_test()).to(eq("this is an Anagram"))
+    test = AnagramTest.new("tea", "eta")
+    expect(test.anagram_test()).to(eq("this is an Anagram"))
   end
   it("continue to test array length") do
-    word2 = AnagramTest.new("cat", "tac")
-    expect(word2.anagram_test()).to(eq("this is an Anagram"))
+    test2 = AnagramTest.new("cat", "tac")
+    expect(test2.anagram_test()).to(eq("this is an Anagram"))
   end
   it("will test if catches Anti-gram") do
-    word3 = AnagramTest.new("adfea", "eat")
-    expect(word3.anagram_test()).to(eq("this is an Anti-gram"))
+    test3 = AnagramTest.new("adfea", "eat")
+    expect(test3.anagram_test()).to(eq("this is an Anti-gram"))
   end
   it("will test if catches Anti-gram") do
-    word4 = AnagramTest.new("tea", "eat")
-    expect(word4.vowel_check()).to(eq(2))
-  end
-  it("will test if catches Anti-gram") do
-    word5 = AnagramTest.new("ttttt", "ttttt")
-    expect(word5.real_word_check()).to(eq("this is not a real word, KID"))
+    test4 = AnagramTest.new("tttt", "tttt")
+    expect(test4.anagram_test()).to(eq("this is not a real word, KID"))
   end
 end
