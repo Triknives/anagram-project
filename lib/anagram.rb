550181b64c636f45f4f73db49bde8988a307271a
Triknives <50305423+Triknives@users.noreply.github.com>
2019-12-22 13:03:41 -0800

    update times loop sytnax and logic to pass specs, and updated unit testing

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,20 @@ class AnagramTest
 
   #This while / do below will allow me to run it against the spec "must have a vowel" to be a real word, as vowel count must be > 0
   def anagram_test()
-    i = 0
-    while i < @word_arr1.length do
-      if @word_arr1[i] == "a" || @word_arr1[i] == "e" || @word_arr1[i] == "i" || @word_arr1[i] == "o" || @word_arr1[i] == "u"
+
+    @word_arr1.length.times do |vowel|
+      if @word_arr1.include?  "a"|| "e" || "i" || "o" || "u"
         @vowel_count += 1
       end
-      i += 1
     end
+
+    # i = 0
+    # while i < @word_arr1.length do
+    #   if @word_arr1[i] == "a" || @word_arr1[i] == "e" || @word_arr1[i] == "i" || @word_arr1[i] == "o" || @word_arr1[i] == "u"
+    #     @vowel_count += 1
+    #   end
+    #   i += 1
+    # end
 
     #Will now take vowel count and run it against our "real word" conditional.
     if @vowel_count == 0
